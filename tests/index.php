22c7ffa5af8fdd80beb69448fa4534cfd1484635
Marco <marco1@live.de>
2016-06-06 09:51:11 +0200

    Improve example in 'tests'

--- tests/index.php
+++ tests/index.php
@@ -20,16 +20,18 @@ header('Content-type: text/html; charset=utf-8');
 
 require __DIR__.'/../src/Router.php';
 
-$router = new \Delight\Router\Router('/tests');
+$router = new \Delight\Router\Router('/PHP-Router/tests');
 
 $router->get('/', function () {
 	echo '<h1>Welcome</h1>';
-	echo '<p>Lorem ipsum</p>';
+	echo '<p>Hello world</p>';
+
 	// use some values from `$_GET` here
 });
 $router->get('/user/:id/:name', function ($id, $name) {
 	echo '<h1>Profile: '.htmlspecialchars($name).'</h1>';
 	echo '<p>My user ID is '.intval($id).'</p>';
+
 	// you may use `$_GET` in addition to the callback arguments
 });
 $router->post('/sign_up', function () {
